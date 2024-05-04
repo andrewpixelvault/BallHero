8b7b11c5626e4bf6b8b07d0b32509dd866beab42
andrewwildsky <andrewccccc123@gmail.com>
2024-05-04 16:14:51 -0700

    new
    new

--- TemplateData/connectMetaMask.js
+++ TemplateData/connectMetaMask.js
@@ -1,13 +1,47 @@
+async function loadABI() {
+    const response = await fetch('path/to/MetaHeroABI.json');
+    const abi = await response.json();
+    return abi;
+}
+
 async function connectMetaMask() {
     if (typeof window.ethereum !== 'undefined') {
         try {
             const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-            SendMessage('WalletConnetButton', 'ReceiveAccount', accounts[0]);
+            SendMessage('WalletManager', 'ReceiveAccount', accounts[0]);
         } catch (error) {
             console.error('User denied account access');
         }
     } else {
         console.error('MetaMask is not installed!');
+    }
+}
+
+async function getContract() {
+    const abi = await loadABI();  
+    const contractAddress = "0x6dc6001535e15b9def7b0f6a20a2111dfa9454e2"; 
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const contract = new ethers.Contract(contractAddress, abi, provider.getSigner());
+    return contract;
+}
+
+
+async function fetchAllNFTsAndSendToUnity() {
+    try {
+        const contract = await getContract();
+        const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+        const accountAddress = accounts[0];
+        const nftCount = await contract.balanceOf(accountAddress);
+        let nftIds = [];
+
+        for (let i = 0; i < nftCount; i++) {
+            const tokenId = await contract.tokenOfOwnerByIndex(accountAddress, i);
+            nftIds.push(tokenId.toString());
+        }
+
+        SendMessage('WalletManager', 'ReceiveNFTIds', JSON.stringify(nftIds));
+    } catch (error) {
+        console.error('Failed to fetch NFTs or send data to Unity:', error);
     }
 }
 
