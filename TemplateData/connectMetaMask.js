feae78f871a6d5576ba0c8c7b342a27b8a37ffc8
andrewwildsky <andrewccccc123@gmail.com>
2024-05-04 17:07:39 -0700

    new

--- TemplateData/connectMetaMask.js
+++ TemplateData/connectMetaMask.js
@@ -8,19 +8,23 @@ async function connectMetaMask() {
     if (typeof window.ethereum !== 'undefined') {
         try {
             const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-            SendMessage('WalletManager', 'ReceiveAccount', accounts[0]);
+            if (accounts.length > 0) {
+                SendMessage('WalletManager', 'ReceiveAccount', accounts[0]);
+            } else {
+                console.log("No accounts received.");
+            }
         } catch (error) {
-            console.error('Failed to access user account:', error);
-            console.log(error); 
+            console.error('User denied account access or an error occurred:', error);
         }
     } else {
         console.error('MetaMask is not installed!');
     }
 }
 
+
 async function getContract() {
-    const abi = await loadABI();  
-    const contractAddress = "0x6dc6001535e15b9def7b0f6a20a2111dfa9454e2"; 
+    const abi = await loadABI();
+    const contractAddress = "0x6dc6001535e15b9def7b0f6a20a2111dfa9454e2";
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const contract = new ethers.Contract(contractAddress, abi, provider.getSigner());
     return contract;
@@ -48,11 +52,13 @@ async function fetchAllNFTsAndSendToUnity() {
 
 
 ethereum.on('accountsChanged', (accounts) => {
-    if (accounts.length === 0) {
-        console.log('Please connect to MetaMask.');
-    } else {
-        SendMessage('WalletManager', 'ReceiveAccount', accounts[0]);
-    }
+
+        if (accounts.length === 0) {
+            console.log('Please connect to MetaMask.');
+        } else {
+            SendMessage('WalletManager', 'ReceiveAccount', accounts[0]);
+        }
+
 });
 
 ethereum.on('chainChanged', (_chainId) => window.location.reload());
